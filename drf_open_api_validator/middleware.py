f8f56784d6e24e0883708df2590550f4ff97ff48
TakesxiSximada <8707279+TakesxiSximada@users.noreply.github.com>
2021-09-23 10:07:38 +0900

    Fix log message.

--- drf_open_api_validator/middleware.py
+++ drf_open_api_validator/middleware.py
@@ -1,6 +1,5 @@
 import logging
 
-import django
 from django.conf import settings
 from django.http import HttpRequest, JsonResponse
 from openapi_core.contrib.django.requests import DjangoOpenAPIRequestFactory
@@ -28,7 +27,6 @@ class DjangoOpenAPIRequestValidationMiddleware:
         self.strict_response_schema = settings.OPENAPI_STRICT_RESPONSE_SCHEMA
 
     def __call__(self, request: HttpRequest):
-        # Check path
         try:
             openapi_request = self.request_factory.create(request)
         except Exception:
@@ -46,26 +44,28 @@ class DjangoOpenAPIRequestValidationMiddleware:
                         status=404,
                     )
             except Exception:
-                logger.warning("Invalid path", exc_info=True)
+                logger.warning("Invalid path: unknown error", exc_info=True)
                 if self.strict_path:
-                    return JsonResponse(
-                        data={"errors": ["unknown"]},
-                        status=404,
-                    )
+                    raise
 
-        if openapi_request:
-            openapi_result = self.request_validator.validate(openapi_request)
-            if openapi_result.errors:
-                logger.warning("Invalid schema, but ingore: %s", openapi_result.errors)
+            try:
+                openapi_result = self.request_validator.validate(openapi_request)
+                if openapi_result.errors:
+                    logger.warning("Invalid schema: %s", openapi_result.errors)
+                    if self.strict_request_schema:
+                        return JsonResponse(
+                            data={
+                                "errors": [str(err) for err in openapi_result.errors],
+                            },
+                            status=400,
+                        )
+            except Exception:
+                logger.warning("Invalid schema: unknwon error", exc_info=True)
                 if self.strict_request_schema:
-                    return JsonResponse(
-                        data={
-                            "errors": [str(err) for err in openapi_result.errors],
-                        },
-                        status=400,
-                    )
+                    raise
 
         response = self.get_response(request)
+
         if openapi_request:
             try:
                 openapi_response = self.response_factory.create(response)
@@ -83,7 +83,7 @@ class DjangoOpenAPIRequestValidationMiddleware:
                         )
             except Exception:
                 logger.warning(
-                    "An error occured in the open api validator", exc_info=True
+                    "Invalid response: unknown error", exc_info=True
                 )
                 if self.strict_response_schema:
                     raise
