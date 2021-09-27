ee82f9918bf91b736d5d5f10f0ca44db426480ff
TakesxiSximada <8707279+TakesxiSximada@users.noreply.github.com>
2021-09-27 09:03:57 +0900

    Add tests.

--- drf_open_api_validator/middleware.py
+++ drf_open_api_validator/middleware.py
@@ -27,31 +27,32 @@ class DjangoOpenAPIRequestValidationMiddleware:
         self.strict_response_schema = settings.OPENAPI_STRICT_RESPONSE_SCHEMA
 
     def __call__(self, request: HttpRequest):
+        openapi_request = None
+        openapi_result_path = None
+
         try:
             openapi_request = self.request_factory.create(request)
         except Exception:
             logger.warning("An error occured in the open api validator", exc_info=True)
-            openapi_request = None
 
         if openapi_request:
             try:
-                self.path_finder.find(openapi_request)
+                openapi_result_path = self.path_finder.find(openapi_request)
+                openapi_result = self.request_validator.validate(openapi_request)
             except (PathNotFound, OperationNotFound, ServerNotFound) as err:
                 logger.warning("Invalid path", exc_info=True)
                 if self.strict_path:
                     return JsonResponse(
-                        data={"errors": [str(err)]},
+                        data={"errors": ["Not found"]},
                         status=404,
                     )
             except Exception:
                 logger.warning("Invalid path: unknown error", exc_info=True)
                 if self.strict_path:
                     raise
-
-            try:
-                openapi_result = self.request_validator.validate(openapi_request)
+            else:
                 if openapi_result.errors:
-                    logger.warning("Invalid schema: %s", openapi_result.errors)
+                    logger.warning("Invalid request: %s", openapi_result.errors)
                     if self.strict_request_schema:
                         return JsonResponse(
                             data={
@@ -59,14 +60,10 @@ class DjangoOpenAPIRequestValidationMiddleware:
                             },
                             status=400,
                         )
-            except Exception:
-                logger.warning("Invalid schema: unknwon error", exc_info=True)
-                if self.strict_request_schema:
-                    raise
 
         response = self.get_response(request)
 
-        if openapi_request:
+        if openapi_request and openapi_result_path:
             try:
                 openapi_response = self.response_factory.create(response)
                 openapi_result = self.response_validator.validate(
